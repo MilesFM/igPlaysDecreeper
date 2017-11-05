3c72d4cba309103a52f3de09ea1d3bd9826021fd
BuildTools <unconfigured@null.spigotmc.org>
2017-11-05 14:33:30 +1100

    New creepy messages

--- gameObjects/creepyMessages.js
+++ gameObjects/creepyMessages.js
@@ -24,12 +24,37 @@ let creepyMsgs = {
     },
     city : class extends creepyMessage {
         constructor() {
-            super("Where does ig live?", "blue", 4);
+            super("Where does ig live?", "cyan", 4);
         }
     },
-    thing : class extends creepyMessage {
+    computer : class extends creepyMessage {
         constructor() {
-            super("What is ig", "cyan", 5);
+            super("What is ig's computing?", "blue", 5);
+        }
+    },
+    subscribe : class extends creepyMessage {
+        constructor() {
+            super("Can ig subscribe to me?", "purple", 6);
+        }
+    },
+    friend : class extends creepyMessage {
+        constructor() {
+            super("Can ig and me be friends?", "yellow", 7);
+        }
+    },
+    face : class extends creepyMessage {
+        constructor() {
+            super("What does ig look like?", "orange", 8);
+        }
+    },
+    identity : class extends creepyMessage {
+        constructor() {
+            super("What is ig's full name?", "tomato", 9);
+        }
+    },
+    live : class extends creepyMessage {
+        constructor() {
+            super("WHERE DOES IG LIVE?!", "red", 10);
         }
     },
 };
